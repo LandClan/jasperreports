218588d4162d7af8c852c448224861919490e124
Chris James <chris@landclan.com>
2024-03-28 12:13:48 +0000

    Updated the scripts that edit the Jaspersoft Studio installation, and added TODOs to add additional processing necessary for updating the new plugin jar that this version of Jaspersoft Studio depends on.

--- jasperreports/enable-landclan-version-in-js.bat
+++ jasperreports/enable-landclan-version-in-js.bat
@@ -1,19 +1,19 @@
 @echo off
-rem This script will need to be run with Administrator privileges if your TIBCO Jaspersoft Studio install location is
+rem This script will need to be run with Administrator privileges if your Jaspersoft Studio install location is
 rem under Administrator control on your system (which is probably true if you installed it to Program Files).
 
-rem This script will alter your local installation of TIBCO Jaspersoft Studio. Please ensure you read these
+rem This script will alter your local installation of Jaspersoft Studio. Please ensure you read these
 rem instructions first so that you know what has changed.
 
 rem This script takes the custom LandClan jar and overwrites the local official jar that is used by Jaspersoft Studio
-rem so that JS runs with the custom LandClan jar.
+rem so that the studio runs with the custom LandClan jar.
 
 rem The install location on my local machine of the relevant jar file is:
-rem C:\Program Files\TIBCO\Jaspersoft Studio-6.17.0\configuration\org.eclipse.osgi\68\0\.cp\lib\jasperreports-6.17.0.jar
+rem C:\Program Files\Jaspersoft\Jaspersoft Studio-6.21.2\configuration\org.eclipse.osgi\73\0\.cp\lib\jasperreports-6.21.2.jar
 
 rem If we do not already have a backup of the official build, make one now.
-set jarDir="C:\Program Files\TIBCO\Jaspersoft Studio-6.17.0\configuration\org.eclipse.osgi\68\0\.cp\lib\"
-set jarFile="jasperreports-6.17.0.jar"
+set jarDir="C:\Program Files\Jaspersoft\Jaspersoft Studio-6.21.2\configuration\org.eclipse.osgi\73\0\.cp\lib\"
+set jarFile="jasperreports-6.21.2.jar"
 set backupFile="%jarFile%.official"
 echo Checking for backup copy of official jar...
 if exist %jarDir%%backupFile% (
@@ -24,5 +24,13 @@ if exist %jarDir%%backupFile% (
 )
 
 echo Copying the custom LandClan version of Jasper Reports to the Jaspersoft Studio library...
-call copy ".\dist\jasperreports-6.17.0-landclan.jar" %jarDir%%jarFile%
+call copy ".\dist\jasperreports-6.21.2-landclan.jar" %jarDir%%jarFile%
+
+rem TODO: Automate the processing of plugin jar C:\Program Files\Jaspersoft\Jaspersoft Studio-6.21.2\plugins\net.sf.jasperreports_6.21.2.final.jar
+rem TODO: If there is not an official backup of the plugin jar, backup the existing plugin jar.
+rem TODO: Copy the official backup of the plugin jar to a temporary directory.
+rem TODO: Within the copy of the plugin jar, replace lib\jasperreports-6.21.2.jar with the landclan version, keeping the
+rem        original name from the plugin source.
+rem TODO: Replace the plugin jar in the plugins directory with the updated plugin jar.
+
 echo Finished.
--- jasperreports/enable-official-version-in-js.bat
+++ jasperreports/enable-official-version-in-js.bat
@@ -1,21 +1,21 @@
 @echo off
-rem This script will need to be run with Administrator privileges if your TIBCO Jaspersoft Studio install location is
+rem This script will need to be run with Administrator privileges if your Jaspersoft Studio install location is
 rem under Administrator control on your system (which is probably true if you installed it to Program Files).
 
 rem This script will alter your local installation of TIBCO Jaspersoft Studio. Please ensure you read these
 rem instructions first so that you know what has changed.
 
 rem This script takes the backup of the official Jasper Reports jar file that was created when the counterpart batch
-rem script 'enable-landclan-version-in-js.bat' was run and re-instates it as the main jar file so that JS runs with the
-rem official jar.
+rem script 'enable-landclan-version-in-js.bat' was run and re-instates it as the main jar file so that Jaspersoft
+rem Studio runs with the official jar.
 
 rem The install location on my local machine of the relevant jar file is:
-rem C:\Program Files\TIBCO\Jaspersoft Studio-6.17.0\configuration\org.eclipse.osgi\68\0\.cp\lib\jasperreports-6.17.0.jar
+rem C:\Program Files\Jaspersoft\Jaspersoft Studio-6.21.2\configuration\org.eclipse.osgi\73\0\.cp\lib\jasperreports-6.21.2.jar
 
 rem If we do not already have a backup of the official build then it is to be assumed that the counterpart batch script
 rem has not been run so there is nothing to do.
-set jarDir="C:\Program Files\TIBCO\Jaspersoft Studio-6.17.0\configuration\org.eclipse.osgi\68\0\.cp\lib\"
-set jarFile="jasperreports-6.17.0.jar"
+set jarDir="C:\Program Files\Jaspersoft\Jaspersoft Studio-6.21.2\configuration\org.eclipse.osgi\73\0\.cp\lib\"
+set jarFile="jasperreports-6.21.2.jar"
 set backupFile="%jarFile%.official"
 echo Checking for backup copy of official jar...
 if exist %jarDir%%backupFile% (
@@ -27,3 +27,5 @@ if exist %jarDir%%backupFile% (
 ) else (
     echo Official version was already enabled (no backup file was found so this is assumed.)
 )
+
+rem TODO: Re-instate the official plugin jar
